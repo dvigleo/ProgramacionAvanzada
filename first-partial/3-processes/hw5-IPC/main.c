913cd85b65c6aaa776cc685f425ac2e1c9d2b68e
Dani Vignau <dvigleo@gmail.com>
2020-09-17 11:54:43 -0500

    Final version of homework 5

--- first-partial/3-processes/hw5-IPC/main.c
+++ first-partial/3-processes/hw5-IPC/main.c
@@ -1,4 +1,6 @@
 /*
+    --* Hecho en conjunto con Hector Alexis Reyes Manrique (A01339607) *--
+    
     Alumno: Daniela Vignau León
     Fecha: 17 de septiembre de 2020
     Materia: Programación Avanzada
@@ -13,44 +15,47 @@
 #include <sys/wait.h>
 #include <string.h>
 
+#define R 0
+#define W 1
+
+typedef struct {
+    int fd[2];
+} pipe_t;
+
 void print_help();
 
 int main(int argc, char * const * argv) {
     char * input = NULL;
-    int argument, start = 0, index;
-    int n = 0;
+    int argument, start = 0, index, n;
     
-    while((argument = getopt (argc, argv, "n:h")) != -1)
+    while((argument = getopt (argc, argv, "n:")) != -1)
     
     switch(argument) {
         case 'n':
             input = optarg;
             if(isdigit(*input)) {
                 n = atoi(input);
-                start = 1;
+                if(n <= 1) {
+                    fprintf(stderr, "-----------------------\nOpción -n require un número >= 2 como argumento\n");
+                    return 1; 
+                } else {
+                    start = 1;
+                }
             } else {
-                fprintf(stderr, "-----------------------\nOpción -%s require un número entero como argumento\n", optarg);
+                fprintf(stderr, "-----------------------\nOpción -n require un número entero >=2 como argumento\n");
             }
-            break;
-        case 'h':
-            print_help();
             break;
         case '?':
             if(optopt == 'n')
-                fprintf(stderr, "-----------------------\nOpción -%c require un número entero como argumento\n", optopt);
+                fprintf(stderr, "-----------------------\nOpción -%c require un número >= 2 entero como argumento\n", optopt);
             else if(isprint (optopt))
-                fprintf(stderr, "-----------------------\nOpción desconocida '-%c\n", optopt);
+                fprintf(stderr, "-----------------------\nOpción desconocida '-%c'\n", optopt);
+                print_help();
             return 1;
         default:
             abort();
     }
     
-    for (index = optind; index < argc; index++) {
-        printf ("-----------------------\nEl argumento %s no es una opción válida", argv[index]);
-        print_help();
-        exit(-1);
-    }
-        
     if (argc != 3) {
         printf("-----------------------\nError");
         print_help();
@@ -58,100 +63,75 @@ int main(int argc, char * const * argv) {
     }
     
     if(start == 1) {
+        int current_lap = 1, i;
+        char message = 'T', m;
         pid_t pid;
-        int i = 0;
-        char m = 'T';
-        char t;
-        int n_children = 0;
         
-        // Create a 2D array of pipes and initiliaze them    
-        int * pipes = (int *) malloc(sizeof(*pipes) * (2 * n));
-        int * aux = pipes;
-        int * end = pipes + n;
+        pipe_t * pipes = (pipe_t *) malloc(sizeof(pipe_t) * n);
+        pipe_t * aux = pipes;
         
-        for(; i < n; ++i) {
-            pipe(&pipes[2 * i]);
-        }
-        printf("\nSucess! %d pipes have been created\n", n);
-    
-        // Write the message to the final pipe so the parent can read it and then send it
-        int err_write = write(end[1], &m, sizeof(char)); 
-        if(err_write == -1){
-            printf("Error");
-        } else {
-            printf("I have written the first message: '%c'\n", m);
+        for(; aux < pipes + n; aux++) {
+            if(pipe(aux->fd) == -1) {
+                printf("Error en creación de pipes");
+                exit(-1);
+            }
         }
         
-        printf("\nParent ID = %d\n", getpid());
-        
-        int start_round = 1;
-        i = 0;
-        while(i < (n - 1)) {
+        // Craete n children
+        for (i = 0; i < n - 1; i++) {
             pid = fork();
-            switch(pid) {
-                case -1: 
-                    printf("Error. %d procesos hijos creados\n", i + 1);
+            switch (pid) {
+                case -1:
+                    printf("Error. %d hijos creados\n", i + 1);
                     exit(-1);
                 case 0: // Child
-                    while(1) {
-                        close(aux[1]); // Close writing end
-                        read(aux[0], &t, sizeof(char));
-                        printf("--> I am PID = %d, and have read the message %c. I'll keep it with me for 5 seconds.\n", getpid(), t);
-                        sleep(3);
+                    while (1) {
+                        // Close all other reading pipes
+                        for(int j = 0; j < n - 1; j++) {
+                            if((pipes + j)->fd[R] != (pipes + i)->fd[R]) {
+                                close((pipes + j)->fd[R]);
+                            }
+                        }
+                        // Read from pipe
+                        close((pipes + i)->fd[W]);
+                        read((pipes + i)->fd[R], &m, sizeof(char));
+                        printf("--> Soy el proceso con PID %d y recibí el testigo %c, el cual tendré por 5 segundos\n", getpid(), m);
                         
-                        // Send the message
-                        aux++;
-                        close(aux[0]); // Close reading end
-                        write(aux[1], &t, sizeof(char));
-                        printf("<-- I am PID = %d, and I have sent the message\n\n", getpid());
-                        n_children++;
-                        // exit(0);
-                        printf("\n\nChildren %d\n\n", n_children);
-                        if(n_children == (n - 1)) {
-                            start_round = 1;
-                            n_children = 0;
-                            exit(0);
-                        }
+                        sleep(5);
+                        
+                        // Write into pipe
+                        close((pipes + i + 1)->fd[R]);
+                        printf("<-- Soy el proceso con PID %d y acabo de enviar el testigo %c\n", getpid(), m);
+                        write((pipes + i + 1)->fd[W], &m, sizeof(char));
                     }
-                    
-                default: // Parent
-                    // while(1){
-                    
-                        if(start_round == 1 ) {
-                            aux = end; // This is necessary, otherwise the parent would not be able to read the first message
-                            start_round = 0;
-                            // Read and receive the message
-                            close(aux[1]); // Close writing end from last child
-                            read(aux[0], &t, sizeof(char));
-                            printf("Parent --> I am PID = %d, and have read the message %c. I'll keep it with me for 5 seconds.\n", getpid(), t);
-                            sleep(3);
-                                
-                            // Send the message
-                            aux = pipes; // Restart pointer
-                            close(aux[0]); // Close reading end
-                            write(aux[1], &t, sizeof(char));
-                            printf("<-- I am PID = %d, and I have sent the message\n\n", getpid());
-                            wait(NULL);                        
-                        } else {
-                            wait(NULL);
-                        }
-                    // }
-            
-                    
-            }
-            i++;
+            } 
         }
         
-        printf("\n\nI have created %d children and 1 parent\n", i);
-        free(pipes);
-        // EOP
+        // The parent should write the first message
+        printf("\nVuelta #%d\n", current_lap);
+        printf("<-- Soy el proceso padre con PID %d y acabo de enviar el testigo %c\n", getpid(), message);
+        write(pipes->fd[W], &message, sizeof(char));
+        
+        while (1) {
+            // Read from last pipe
+            close((pipes + 1)->fd[W]);
+            read((pipes + n - 1)->fd[R], &m, sizeof(char));
+            printf("--> Soy el proceso padre con PID %d y recibí el testigo %c, el cual tendré por 5 segundos\n", getpid(), m);
+            
+            sleep(5);
+            
+            // Write to first pipe
+            close(pipes->fd[R]);
+            current_lap++;
+            printf("\nVuelta #%d\n", current_lap);
+            printf("<-- Soy el proceso padre con PID %d y acabo de enviar el testigo %c\n", getpid(), m);
+            write(pipes->fd[W], &m, sizeof(char));
+        }
     }
-    
     return 0;
 }
 
 void print_help() {
-    printf("\nUse: ./a.out [-n value] [-h]\n");
-    printf("Opciones:\n");
-    printf("\t-n: Crear n procesos indicados como valor entero\n\t h: Ayuda\n");
+    printf("\nUse: ./a.out -n [entero]\n");
+    printf("   Donde: [entero] >= 2\n");
 }
